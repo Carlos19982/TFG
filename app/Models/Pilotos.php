07682bb8f25d85fd2645d29b1a4b9dd763df3a58
Carlos <carlos.blanco.hidalgo1998@gmail.com>
2025-05-01 12:39:39 +0200

    Migracion y modelo de piloto

--- app/Models/Pilotos.php
+++ app/Models/Pilotos.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Pilotos extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'pilotos';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $fillable = [
+        'nombre',
+        'apellidos',
+        'Descripcion'
+    ];
+
+    public function eventos(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Eventos::class,           // Modelo relacionado
+            'Piloto_Evento',         // Tabla pivote
+            'piloto_id',             // Clave foránea de Piloto en tabla pivote
+            'evento_id'              // Clave foránea de Evento en tabla pivote
+        )->withPivot('id','FechaRegistro'); // Incluir campo extra de la tabla pivote
+           
+    }
 }
