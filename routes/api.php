48422769172c5d69182b53152c7a61bb7b5cc9e8
Carlos <carlos.blanco.hidalgo1998@gmail.com>
2025-05-03 13:14:17 +0200

    Controlador pilotos y enrutamiento

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ThemeSettingsController;
+use App\Http\Controllers\Api\PilotController;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/theme-settings', [ThemeSettingsController::class, 'updateSettings']);
+Route::get('/pilots', [PilotController::class, 'index']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\PilotController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +15,7 @@ Route::get('/calendario', function () {
 Route::get('/calendario/informacion', function () {
     return view('informacion');
 })->name('calendario.informacion');
+
 
 Route::get('/pilotos', function () {
     return view('pilotos');
